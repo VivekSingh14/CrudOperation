1b91f4f8a9e3d9b464365225979ec8c48b8386c4
Vivek Singh <vivek.singh141@yahoo.com>
2024-04-19 17:25:18 +0530

    chore: new application for learning postgres as backend

--- models/model.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type MonumentAttribute struct {
-	Id      string `json:"id"`
-	Name    string `json:"name"`
-	Country string `json:"country"`
-	IsOpen  bool   `json:"isOpen"`
-}
--- server/handler.go
+++
@@ -1,7 +0,0 @@
-package server
-
-import "github.com/CrudOperation/handler"
-
-func (s *Server) registerHandler() {
-	handler.RegisterHandlers(s.router, s.config)
-}
